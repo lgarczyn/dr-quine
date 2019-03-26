7dd420b93e06346e49ca3d0d3b145faa6c961a18
lgarczyn <lgarczyn@student.42.fr>
2019-03-26 17:44:39 +0100

    fixed JS

--- JS/Colleen.js
+++ JS/Colleen.js
@@ -1,6 +1,8 @@
 main();
 // comment 1
 function main() {
+    function fn() {}
+    fn();
     // comment 2
     console.log("main();\n// comment 1\n" + main.toString());
 }
--- JS/Grace.js
+++ JS/Grace.js
@@ -1,4 +1,5 @@
-var A = "var f = String.fromCharCode; var fs = require('fs');fs.writeFile('./Grace_kid.js', 'var A = ' + f(34) + A + f(34) + ';' + f(10) + 'var B = A;' + f(10) + 'var C = A;' + f(10) + 'eval(A);' + f(10), () => {});";
+var A = "var f = String.fromCharCode; var fs = require('fs');fs.writeFile('./Grace_kid.js', 'var A = ' + f(34) + A + f(34) + ';' + f(10) + 'var B = A;' + f(10) + 'var C = A;' + f(10) + 'eval(A);' + f(10) + '//comment' + f(10), () => {});";
 var B = A;
 var C = A;
 eval(A);
+//comment
