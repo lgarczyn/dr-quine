9612a652cd9e2945ca4d21b497ba237affada076
lgarczyn <lgarczyn@student.42.fr>
2019-04-05 12:10:23 +0200

    fixed non-negative non-5 numbers

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -7,9 +7,8 @@ extern system
 global main
 
 section .data
-code db "extern dprintf%2$cextern sprintf%2$cextern access%2$cextern open%2$cextern close%2$cextern system%2$cglobal main%2$c%2$csection .data%2$ccode db %3$c%1$s%3$c, 0%2$ccmd db %3$cS=Sully_%3$c, 37, %3$cd && nasm $S.s -f macho64 --prefix _ && gcc $S.o -o $S && rm $S.o && chmod 700 $S && ./$S%3$c, 10, 0%2$cfirst db %3$cSully_5.s%3$c, 0%2$cname db %3$cSully_%3$c, 37, %3$cd.s%3$c, 0%2$cbuffer: TIMES 1024 db 0%2$c%2$csection .text%2$cmain:%2$cpush RBP%2$cpush R12%2$cpush R13%2$cmov R12, %4$d%2$c;checking counter and existing Sully_5, then dec counter%2$ccmp R12, 0%2$cjle _end%2$c%2$clea RDI, [rel first]%2$cmov RSI, 0%2$ccall access%2$ccmp EAX, 0%2$cjnz _endif%2$cdec R12%2$c%2$c_endif:%2$c;getting file name%2$clea RDI, [rel buffer]%2$clea RSI, [rel name]%2$cmov RDX, R12%2$cxor RAX, RAX%2$ccall sprintf%2$c%2$c;opening file%2$clea RDI, [rel buffer]%2$cmov RSI, 01001o%2$cmov RDX, 0644o%2$cmov RAX, 2%2$ccall open%2$c%2$c;writing to file%2$cmov RDI, RAX%2$clea RSI, [rel code]%2$clea RDX, [rel code]%2$cmov RCX, 10%2$cmov R8, 34%2$cmov R9, R12%2$cxor RAX, RAX%2$ccall dprintf%2$ccall close%2$c%2$c;getting cmd%2$clea RDI, [rel buffer]%2$clea RSI, [rel cmd]%2$cmov RDX, R12%2$cxor RAX, RAX%2$ccall sprintf%2$c%2$c;executing%2$clea RDI, [rel buffer];?%2$ccall system%2$c%2$c_end:%2$cmov RAX,0%2$cpop R13%2$cpop R12%2$cpop RBP%2$cret%2$c", 0
+code db "extern dprintf%2$cextern sprintf%2$cextern access%2$cextern open%2$cextern close%2$cextern system%2$cglobal main%2$c%2$csection .data%2$ccode db %3$c%1$s%3$c, 0%2$ccmd db %3$cS=Sully_%3$c, 37, %3$cd && nasm $S.s -f macho64 --prefix _ && gcc $S.o -o $S && rm $S.o && chmod 700 $S && ./$S%3$c, 10, 0%2$cname db %3$cSully_%3$c, 37, %3$cd.s%3$c, 0%2$cbuffer: TIMES 1024 db 0%2$c%2$csection .text%2$cmain:%2$cpush RBP%2$cpush R12%2$cpush R13%2$cmov R12, %4$d%2$c;checking counter and existing Sully_5, then dec counter%2$ccmp R12, 0%2$cjle _end%2$c%2$c;getting current name%2$clea RDI, [rel buffer]%2$clea RSI, [rel name]%2$cmov RDX, R12%2$cxor RAX, RAX%2$ccall sprintf%2$c%2$c;checking if current file exists as it should%2$clea RDI, [rel buffer]%2$cmov RSI, 0%2$ccall access%2$ccmp EAX, 0%2$c%2$c;dec counter only if it doesn't%2$cjnz _endif%2$cdec R12%2$c_endif:%2$c%2$c;getting file name%2$clea RDI, [rel buffer]%2$clea RSI, [rel name]%2$cmov RDX, R12%2$cxor RAX, RAX%2$ccall sprintf%2$c%2$c;opening file%2$clea RDI, [rel buffer]%2$cmov RSI, 01001o%2$cmov RDX, 0644o%2$cmov RAX, 2%2$ccall open%2$c%2$c;writing to file%2$cmov RDI, RAX%2$clea RSI, [rel code]%2$clea RDX, [rel code]%2$cmov RCX, 10%2$cmov R8, 34%2$cmov R9, R12%2$cxor RAX, RAX%2$ccall dprintf%2$ccall close%2$c%2$c;getting cmd%2$clea RDI, [rel buffer]%2$clea RSI, [rel cmd]%2$cmov RDX, R12%2$cxor RAX, RAX%2$ccall sprintf%2$c%2$c;executing%2$clea RDI, [rel buffer]%2$ccall system%2$c%2$c_end:%2$cmov RAX,0%2$cpop R13%2$cpop R12%2$cpop RBP%2$cret%2$c", 0
 cmd db "S=Sully_", 37, "d && nasm $S.s -f macho64 --prefix _ && gcc $S.o -o $S && rm $S.o && chmod 700 $S && ./$S", 10, 0
-first db "Sully_5.s", 0
 name db "Sully_", 37, "d.s", 0
 buffer: TIMES 1024 db 0
 
@@ -23,14 +22,24 @@ mov R12, 5
 cmp R12, 0
 jle _end
 
-lea RDI, [rel first]
+;getting current name
+lea RDI, [rel buffer]
+lea RSI, [rel name]
+mov RDX, R12
+xor RAX, RAX
+call sprintf
+
+;checking if current file exists as it should
+lea RDI, [rel buffer]
 mov RSI, 0
 call access
 cmp EAX, 0
+
+;dec counter only if it doesn't
 jnz _endif
 dec R12
-
 _endif:
+
 ;getting file name
 lea RDI, [rel buffer]
 lea RSI, [rel name]
@@ -64,7 +73,7 @@ xor RAX, RAX
 call sprintf
 
 ;executing
-lea RDI, [rel buffer];?
+lea RDI, [rel buffer]
 call system
 
 _end:
--- C/Sully.c
+++ C/Sully.c
@@ -1,4 +1,4 @@
-#define F "#define F %3$c%1$s%3$c%2$c#include <stdio.h>%2$c#include <stdlib.h>%2$c#include <unistd.h>%2$c#define S(X) #X%2$cint main()%2$c{%2$cint i = %5$d;%2$cif(i<=0)return(0);%2$cif(access(S(Sully_5.c),F_OK)!=-1) i--;%2$cchar b[1000];%2$csprintf(b, S(Sully_%4$cd.c), i);%2$cFILE* f = fopen(b,S(w));%2$cfprintf(f, F, F, 10, 34, 37, i);%2$cfclose(f);%2$csprintf(b, S(gcc Sully_%4$cd.c -o Sully_%4$cd && chmod 700 Sully_%4$cd && ./Sully_%4$cd), i, i, i, i);%2$csystem(b);%2$c}%2$c"
+#define F "#define F %3$c%1$s%3$c%2$c#include <stdio.h>%2$c#include <stdlib.h>%2$c#include <unistd.h>%2$c#define S(X) #X%2$cint main()%2$c{%2$cint i = %5$d;%2$cif(i<=0)return(0);%2$cchar b[1000];%2$csprintf(b, S(Sully_%4$cd.c), i);%2$cif(access(b, F_OK)!=-1) i--;%2$csprintf(b, S(Sully_%4$cd.c), i);%2$cFILE* f = fopen(b, S(w));%2$cfprintf(f, F, F, 10, 34, 37, i);%2$cfclose(f);%2$csprintf(b, S(gcc Sully_%4$cd.c -o Sully_%4$cd && chmod 700 Sully_%4$cd && ./Sully_%4$cd), i, i, i, i);%2$csystem(b);%2$c}%2$c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,10 +7,11 @@ int main()
 {
 int i = 5;
 if(i<=0)return(0);
-if(access(S(Sully_5.c),F_OK)!=-1) i--;
 char b[1000];
 sprintf(b, S(Sully_%d.c), i);
-FILE* f = fopen(b,S(w));
+if(access(b, F_OK)!=-1) i--;
+sprintf(b, S(Sully_%d.c), i);
+FILE* f = fopen(b, S(w));
 fprintf(f, F, F, 10, 34, 37, i);
 fclose(f);
 sprintf(b, S(gcc Sully_%d.c -o Sully_%d && chmod 700 Sully_%d && ./Sully_%d), i, i, i, i);
